87735b4e81836ced5f829bd0391a81c7fe82af6c
emmanuelU17 <emmanueluluabuike@gmail.com>
2023-09-15 06:56:56 -0400

    validating migration failure

--- src/main/resources/db/migration/V6__init_migration.sql
+++ src/main/resources/db/migration/V6__init_migration.sql
@@ -6,7 +6,7 @@
 # product
 UPDATE product SET uuid=(SELECT uuid()) WHERE uuid IS NULL;
 ALTER TABLE product MODIFY COLUMN uuid varchar(36) NOT NULL UNIQUE;
-CREATE INDEX IX_product_uuid ON product (uuid);
+CREATE INDEX ind_product_uuid ON product (uuid);
 
 # category
 UPDATE product_category SET uuid=(SELECT uuid()) WHERE uuid IS NULL;
